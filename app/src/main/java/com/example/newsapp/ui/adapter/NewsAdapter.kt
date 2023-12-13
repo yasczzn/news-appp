1b317415fe76167458f7d298c28bde9b9a5eed6e
Yasmina Zain <67325050+yasczzn@users.noreply.github.com>
2023-12-13 23:29:53 +0700

    131223 commit
    Setting up NewsAdapter.kt

--- app/src/main/java/com/example/newsapp/ui/adapter/NewsAdapter.kt
+++ app/src/main/java/com/example/newsapp/ui/adapter/NewsAdapter.kt
@@ -3,30 +3,58 @@ package com.example.newsapp.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.newsapp.R
+import com.example.newsapp.data.model.Article
 import com.example.newsapp.data.model.NewsResult
 import com.example.newsapp.databinding.ItemNewsBinding
 
-class NewsAdapter(private val newsList: List<NewsResult>) : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+    class NewsViewHolder(val binding: ItemNewsBinding) : RecyclerView.ViewHolder(binding.root)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val itemBinding = ItemNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return NewsViewHolder(itemBinding)
+        val binding = ItemNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NewsViewHolder(binding)
     }
 
-    class NewsViewHolder(private val itemBinding: ItemNewsBinding) : RecyclerView.ViewHolder(itemBinding.root){
-        fun bind(newsResult: NewsResult) {
-            itemBinding.tvNews.text = newsResult.articles.toString()
-            itemBinding.tvSource.text = newsResult.articles.toString()
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
         }
     }
 
-    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        val newsResult: NewsResult = newsList[position]
-        holder.bind(newsResult)
+    val differ = AsyncListDiffer(this, differCallback)
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
     }
 
-    override fun getItemCount(): Int = newsList.size
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val article = differ.currentList[position]
+
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(holder.binding.imgNews)
+            holder.binding.tvNews.text = article.title
+            holder.binding.tvSource.text = article.source.name
+            holder.binding.tvDesc.text = article.description
+            holder.binding.tvDatetime.text = article.publishedAt
+
+            setOnClickListener {
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
+        }
+    }
 
 }
 
